8f438cf6241d8337aa46f306ea8b6282f0eadb96
Muriuki254 <ianringera@gmail.com>
2023-05-17 00:20:31 +0300

    minor updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import "./main.css"
 
 function Footer() {
+  const date = new Date().toLocaleDateString();
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <h2>© Ian Muriuki</h2>
+      <p> {date} Web Dev News</p>
+    </div>
   )
 }
 
