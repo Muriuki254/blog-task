5a5a6a495399276ac159fee13c889bbc9d4a6a23
Muriuki254 <ianringera@gmail.com>
2023-05-17 09:36:56 +0300

    Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
+import "./main.css"
+import logo from './public/vite.png';
 function Navbar() {
     return (
         <div className="navbar">
+            <img src={logo} alt="" />
+
             <h1>Web Development Technologies </h1>
         </div>
     );
